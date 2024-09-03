4aff9e65e5b874b9d4a56d457d8505e0b8908f97
Erwinher039 <hernandezerwin391@gmail.com>
2024-09-03 14:03:43 -0700

    Add files via upload

---
+++ eva_!_4_tv/src/eva_/_4_tv/Eva__4_tv.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_._4_tv;
+
+/**
+ *
+ * @author Compac
+ */
+public class Eva__4_tv {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      
+          String marca;
+      String modelo;
+      String tipo;
+      int tama;
+      double precio;
+
+      
+      marca = "Sony";
+      modelo = "Bravia";
+      tipo = "QLED";
+      tama = 100;
+      precio = 50000.9;
+              
+              
+      System.out.println(marca);
+      System.out.println(modelo);
+      System.out.println(tipo);
+      System.out.println(tama);
+      System.out.println(precio);
+// TODO code application logic here
+    
+    }
+    
+}
