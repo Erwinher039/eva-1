a04adc784a08d0b348a89db20dfe1701dc46c894
Erwinher039 <hernandezerwin391@gmail.com>
2024-09-03 16:56:12 -0700

    Add files via upload

---
+++ Eva_1_10_operaciones2/src/eva_1_10_operaciones2/Eva_1_10_operaciones2.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_10_operaciones2;
+
+/**
+ *
+ * @author Compac
+ */
+public class Eva_1_10_operaciones2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CONTENACION CADENAS:
+        //OPERACIONES DE CONCATENACION + 
+        //Mismo simbolo con diferentes usos:
+        //sobrecarga de operadores
+        String nombre = "Erwin";
+        String apellido = "Hernandez";
+        String nomComp;
+        //concatenar: unir cadenas de texto
+        nomComp = nombre + " " + apellido;
+        //imprimir los datos:
+        System.out.println("Nombre del usuario:" + nombre );
+        System.out.println("apellido del usuraio" + apellido);
+        System.out.println("nombre completo:" + nomComp);
+        
+    }
+    
+}
