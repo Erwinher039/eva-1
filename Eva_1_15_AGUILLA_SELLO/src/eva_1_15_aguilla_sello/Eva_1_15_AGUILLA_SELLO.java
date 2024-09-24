643bd07ee9fb4e3f172c93a092215c90bc7303d5
Erwinher039 <hernandezerwin391@gmail.com>
2024-09-24 16:33:07 -0700

    Add files via upload

---
+++ Eva_1_15_AGUILLA_SELLO/src/eva_1_15_aguilla_sello/Eva_1_15_AGUILLA_SELLO.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_15_aguilla_sello;
+
+/**
+ *
+ * @author Compac
+ */
+public class Eva_1_15_AGUILLA_SELLO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //0.0 -- 0.99999999999999999
+        //0.5
+        //0 1 2 3 4 5 6 7 8 9
+        System.out.println(Math.random);
+        double valor = Math.random();
+        if(valor >= 0.5){
+            System.out.println("Aguila");
+          }else{
+         System.out.println("sello");
+        }
+    }
+    
+}
