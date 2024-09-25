b1cda84e7b718817a4ed5e3e6c4be8283eeff116
Erwinher039 <hernandezerwin391@gmail.com>
2024-09-25 11:44:55 -0700

    Add files via upload

---
+++ EVA_1_18_Rangos/src/eva_1_18_rangos/EVA_1_18_Rangos.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_18_rangos;
+
+/**
+ *
+ * @author Compac
+ */
+public class EVA_1_18_Rangos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int califa;
+        Scanner capyu = new Scanner(System.in);
+        System.out.println("Introduce una calificacion:");
+        califa = captu.nextlnt();
+        
+        if(califa >= 0 && califa <= 100){
+            System.out.println("CALIFICACION VALIDA");
+        }else{
+            System.out.println("CALIFICACION NO VALIDA");
+    }
+    
+}
