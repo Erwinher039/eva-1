2e95c6b26f857ee277098a3548e5070904b057c3
Erwinher039 <hernandezerwin391@gmail.com>
2024-09-26 10:02:57 -0700

    Add files via upload

---
+++ EVA_1_21_Meses_Año/src/eva_1_21_meses_año/EVA_1_21_Meses_Año.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_1_21_meses_año;
+
+/**
+ *
+ * @author Compac
+ */
+public class EVA_1_21_Meses_Año {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int mes;
+        Scanner captu = new Scanner(System.in);
+        System.out.println("Captura el numero del mes (1 a 12):");
+        mes = capru.nextLine();
+        
+        
+    }
+    
+}
