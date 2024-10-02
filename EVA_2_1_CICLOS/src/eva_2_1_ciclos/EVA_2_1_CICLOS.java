013b211419ff01c6ca876331f45709a9acd8cf83
Erwinher039 <hernandezerwin391@gmail.com>
2024-10-02 16:47:22 -0700

    Add files via upload

---
+++ EVA_2_1_CICLOS/src/eva_2_1_ciclos/EVA_2_1_CICLOS.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_1_ciclos;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_2_1_CICLOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //CICLOS FOR --> CUANDO SABEMOS CUANTAS VECES SE REPITE LA INTRUCCION
+        //FUNCIONA COMO UN CONTADOR: INICIO, FIN (condicion), Y SALTO
+        //1 --> index --> indice
+        //vamos a repetir 10 veces. contando de 1 a 10
+        //NOSOTROS ESTABLECEMOS EL PUNTO DE INICIO Y DE FIN
+        for(int i = 0; i <= 10;i++){
+            System.out.print(1 +" - ");
+        }
+        for(int i = 10; i >= 10;i ++){
+            System.out.print(i + "-");
+    }
+    }
+    
+}
