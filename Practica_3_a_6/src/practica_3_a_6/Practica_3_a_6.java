1cdae2f9593581b900f707172433b88fdf568c0d
Erwinher039 <hernandezerwin391@gmail.com>
2024-10-10 09:36:23 -0700

    Add files via upload

---
+++ Practica_3_a_6/src/practica_3_a_6/Practica_3_a_6.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package practica_3_a_6;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author herna
+ */
+public class Practica_3_a_6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+// TODO code application logic here
+        Scanner sc = new Scanner(System.in);
+        
+                //Solicitar una cadena
+        System.out.print("ingresa cadena de texto");
+        String cadena = sc.nextLine();
+        
+        //imprimir cada caracter en una linea separada
+        for(int i = 0; i < cadena.length(); i++){
+        }
+        sc.close();
+    }
+    
+}
