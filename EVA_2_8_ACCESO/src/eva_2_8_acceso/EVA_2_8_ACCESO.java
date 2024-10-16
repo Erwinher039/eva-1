6643a984e3f160cf272cc2a1a4ca97b32b050ef4
Erwinher039 <hernandezerwin391@gmail.com>
2024-10-16 09:40:14 -0700

    Add files via upload

---
+++ EVA_2_8_ACCESO/src/eva_2_8_acceso/EVA_2_8_ACCESO.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_8_acceso;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_2_8_ACCESO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //pantalla de validacion
+        Scanner captu = new Scanner(System.in);
+        //usuario y contraseña
+        String cont, usua;
+        
+        System.out.println("usuario:");
+        usua=captu.nextLine();
+        System.out.println("contraseña:");
+        cont=captu.nextLine();
+        do{
+            
+        }while(!(usua.equals("admi")&& cont.equals("3321")));
+        }
+    }
+    
+
