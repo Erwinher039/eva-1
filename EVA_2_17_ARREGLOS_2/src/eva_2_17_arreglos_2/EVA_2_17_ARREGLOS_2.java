5da82161474da9a7c514c55bd9c8677ef4a1c606
Erwinher039 <hernandezerwin391@gmail.com>
2024-10-22 18:10:45 -0700

    Add files via upload

---
+++ EVA_2_17_ARREGLOS_2/src/eva_2_17_arreglos_2/EVA_2_17_ARREGLOS_2.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_2_17_arreglos_2;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_2_17_ARREGLOS_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String diasSemana[] = {"Lunes", " Martes", "Miercoles", "Jueves",
+        "Viernes", "Sabado", "Domingo"};
+        int valores[] = {20, 50, 100, 200, 500, 100};
+        for (int i = 0; i < diasSemana.length; i++) {
+            System.out.println(diasSemana[1] + " - ");    
+            }
+        System.out.println("");
+        for (int i = 0; i < valores.length; i++) {
+            System.out.print(valores[1] + " - ");
+        }
+    }
+    
+}
