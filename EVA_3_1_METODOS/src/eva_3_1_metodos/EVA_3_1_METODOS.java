45bdc37bb84ff5e89eccf125e22d7d67c8b8177b
Erwinher039 <hernandezerwin391@gmail.com>
2024-11-04 16:56:08 -0700

    Add files via upload

---
+++ EVA_3_1_METODOS/src/eva_3_1_metodos/EVA_3_1_METODOS.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_1_metodos;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_3_1_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //USO DE METODOS
+        //INVOCACION O LLAMADA
+       imprimirMensaje("Hola mundo!!");
+        
+    }
+    
+    //header (encabezado)
+    //IMPRIMIR UN MENSAJE
+    //JAVA; iniciar en minusculas, verbo, siguientes palabras iniciar
+    public static void imprimirMensaje(String mensaje){
+        System.out.println("TU MENSAJE: " + mensaje);
+    }
+}
