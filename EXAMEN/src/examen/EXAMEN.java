73a9cbea834f629a17a1a53c95b11923d0df4f24
Erwinher039 <hernandezerwin391@gmail.com>
2024-11-08 17:10:31 -0700

    Add files via upload

---
+++ EXAMEN/src/examen/EXAMEN.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package examen;
+
+/**
+ *
+ * @author herna
+ */
+public class EXAMEN {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(piedra = 1);
+        System.out.println(papel = 2);
+        System.out.println(tijera = 3);
+        
+        int juegar [](piedra,papel,tijera)
+            nextLine(ganador = ("piedra", "papel", "tijera",);
+            
+            for introducerGanadoryPerdedor;
+                ganador computadora = (pierda,papel,tijera,);
+                ganador usuario = (papel,tijera,peired);
+                empates = igual (("piedra", pierda, "papel", papel, "tijera", tijera,));
+                        
+    }
+    
+}
