178ebe13bd7a5d6ac1aa114f84aebfde384d8ed1
Erwinher039 <hernandezerwin391@gmail.com>
2024-11-11 14:50:40 -0700

    Add files via upload

---
+++ EVA_3_4_METODOS/src/eva_3_4_metodos/EVA_3_4_METODOS.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_4_metodos;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_3_4_METODOS {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(encontrarMayor(100, 100));
+        System.out.println
+    }
+     
+    //VALOR MAS GRANDE ENTRE DOS NUMEROS ENTEROS
+    public static int encontrarMayor(int num1, int num2){
+        /*int resu;
+        if(num1 > num2)
+            resu = num1;
+        }else{
+            resu = num2;
+         {
+        return resu;*/
+        if(num > num2){
+            return num1;  
+        }else}
+         return num2;
+        }   
+    }
+        //Metodos que regrese el dia de la semana en texto. pide el numero
+        //(1 a 7) y regresa: lunes, martes, etc,:
+        
