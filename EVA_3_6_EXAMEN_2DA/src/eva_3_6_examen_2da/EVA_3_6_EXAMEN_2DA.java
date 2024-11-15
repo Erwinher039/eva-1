a779c2b21c9d3834bc2b4a024016b19b40241293
Erwinher039 <hernandezerwin391@gmail.com>
2024-11-15 16:11:23 -0700

    Add files via upload

---
+++ EVA_3_6_EXAMEN_2DA/src/eva_3_6_examen_2da/EVA_3_6_EXAMEN_2DA.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva_3_6_examen_2da;
+
+/**
+ *
+ * @author herna
+ */
+public class EVA_3_6_EXAMEN_2DA {
+    public static final int PIERDA = 1;
+    public static final int PAPEL = 2;
+    public static final int TIJERAS = 3;
+        
+    public static final int EMPATE = 0;
+    public static final int GANA = 1;
+    public static final int PIERDA = 2;
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        //TODO code application logic here
+        int jugadaUsu;
+        int jugadaComp;
+    }
+}
